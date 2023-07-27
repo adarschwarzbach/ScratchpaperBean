6a2302fe73923a39c128ee203cbcdb28dd9c1db1
Adar Schwarzbach <96645075+adarschwarzbach@users.noreply.github.com>
2023-07-26 19:56:32 -0700

    Add CAFES as base

---
+++ README.md
@@ -0,0 +1,45 @@
+# Spresso Web - An Isotachphoresis Simulator
+
+This project is a web based GPU-accelerated re-implementation of 
+[Spresso](http://stanfordspresso.blogspot.com/) (Stanford Public Release Electrophoretic 
+Sparation Solver). It adopts a pure-frontend implementation that removes the requirement 
+for a backend compute server. This means that all computations run entirely on local browsers,
+providing a maximum degree of real-time user interactivity.
+
+## Implementation Notes
+
+High dimensional array processing is done via 
+[TensorFlow JavaScript](https://www.tensorflow.org/js). A simulation step is defined as
+a TensorFlow computation graph [here](model/spresso_tf.py). The graph is serialized and 
+saved as a [TF SavedModel](https://www.tensorflow.org/guide/saved_model).
+The model is then loaded by the JavaScript frontend, and inference is done by executing
+the graph. The computation can be accelerated on GPU (via WebGL) per device supports, thanks to 
+[tfjs-backend-webgl](https://github.com/tensorflow/tfjs/tree/master/tfjs-backend-webgl).
+CPU acceleration (via WebAssembly) is unfortunately not yet supported. It is mainly 
+awaiting upstream 
+[tfjs-backend-wasm](https://github.com/tensorflow/tfjs/tree/master/tfjs-backend-wasm) 
+supports for `cumsum` operator.
+
+## Project Structure
+
+    |-- public              # directory for all statically served files
+    |   |-- spresso_sim     # serialized simulation model
+    |   |-- spresso_ph      # serialized pH initialization model
+    |-- src                 # all JS / CSS source files
+    |   |-- App.js          # top level simulation UI interface
+    |   |-- Input.js        # encapsulation of commonly used input elements
+    |   |-- Spresso.js      # encapsulation of spresso simulation states management
+    |   |-- worker.js       # a seperate web worker that runs the simulation
+    |-- python              # all Python source files
+    |   |-- spresso_tf.py   # Tensorflow 2.x implementation of Spresso computation graph
+    |   |-- utils.py        # utility functions for post analysis
+    |-- config-overrides.js # react-app-rewired custom configurations
+    |-- package.json        # library dependancies
+
+## Serving
+
+This project is bootstrapped with 
+[Create React App](https://github.com/facebook/create-react-app). Follow their instructions 
+to setup and serve the project.
+
+
---
+++ python/utils.py
@@ -0,0 +1,70 @@
+import base64
+import json
+import os
+
+import numpy as np
+
+class SimResult:
+    """ class abstraction of simulation results """
+
+    def __init__(self, inputs, grid_n, concentration_tsn, cH_tn, efield_tn, time_t):
+        """
+        Args:
+            inputs:             all simulation inputs
+            grid_n:             spatial discretization grid of the channel domain
+            concentration_tsn:  all time slices of concentration matrix in [mole / m^3]
+            cH_tn:              all time slices of Hydrogen ion concentration in [mole / liter]
+            efield_tn:          all time slices of electric field in [V / m]
+            time_t:             simulated time steps
+
+        Note:
+            <variable name>_xyz is a naming convention for an array of shape (x, y, z)
+            Dimension Definition:
+                n:  number of grid points
+                s:  number of species
+                t:  number of time steps
+        """
+        self.inputs = inputs
+        self.grid_n = grid_n
+        self.concentration_tsn = concentration_tsn
+        self.cH_tn = cH_tn
+        self.efield_tn = efield_tn
+        self.time_t = time_t
+
+    @staticmethod
+    def from_directory(directory):
+        """
+        Args:
+            directory: directory that stores the uncompressed result files
+
+        Returns:
+            a parsed SimResult object containing all the simulation result data as well
+            as experimental setup
+        """
+        input_file = os.path.join(directory, "inputs.json")
+        concentration_tsn_file = os.path.join(directory, "concentration_tsn.bin")
+        cH_tn_file = os.path.join(directory, "cH_tn.bin")
+        efield_tn_file = os.path.join(directory, "efield_tn.bin")
+        time_t_file = os.path.join(directory, "time_t.bin")
+
+        with open(input_file, 'r') as f:
+            inputs = json.load(f)
+
+        num_grids = inputs["numGrids"]
+        num_species = len(inputs["species"])
+
+        concentration_tsn = np.fromfile(
+            concentration_tsn_file, dtype=np.float32).reshape(-1, num_species, num_grids)
+        cH_tn = np.fromfile(cH_tn_file, dtype=np.float32).reshape(-1, num_grids)
+        efield_tn = np.fromfile(efield_tn_file, dtype=np.float32).reshape(-1, num_grids)
+        time_t = np.fromfile(time_t_file, dtype=np.float32)
+
+        return SimResult(
+            inputs=inputs,
+            grid_n=np.linspace(0, inputs['domainLen'], inputs['numGrids'], endpoint=False),
+            concentration_tsn=concentration_tsn,
+            cH_tn=cH_tn,
+            efield_tn=efield_tn,
+            time_t=time_t
+        )
+
