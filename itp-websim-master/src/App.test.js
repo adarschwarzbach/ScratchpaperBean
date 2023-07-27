699cfbe73032fd14fbbacb133aa02afca84558a0
Adar Schwarzbach <96645075+adarschwarzbach@users.noreply.github.com>
2023-07-26 19:56:02 -0700

    Remove Cafes

--- itp-websim-master/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders spresso', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Spresso/i);
-  expect(linkElement).toBeInTheDocument();
-});
