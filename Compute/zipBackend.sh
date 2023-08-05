4ac72221d6187862de04a7baba6e1cd121f58859
adarschwarzbach <adar.schwarzbach@gmail.com>
2023-08-05 16:08:21 -0700

    add numpyLambdaLayer

---
+++ Compute/zipBackend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run by:
+#  `chmod +x Compute/zipBackend.sh`
+#  `./Compute/zipBackend.sh`
+
+cd Compute
+
+if [ -f "beanBackend.py" ]; then
+    echo "Zipping beanBackend.py..."
+    zip beanBackend.zip beanBackend.py
+    echo "Zip operation completed."
+else
+    echo "File beanBackend.py not found."
+fi
+
+cd ..
